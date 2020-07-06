64902beee05205ef3418789cb05e1a4e66cbc812
Roberto Meléndez <rcmelendez@users.noreply.github.com>
2020-07-06 10:58:52 -0400

    double brackets is preferred per Bash documentation
    Signed-off-by: Roberto Meléndez <rcmelendez@users.noreply.github.com>

--- conf/install.sh
+++ conf/install.sh
@@ -2,7 +2,7 @@
 
 . /etc/profile
 
-if [ -f /opt/devo/relay-data/selfsigned ]; then
+if [[ -f /opt/devo/relay-data/selfsigned ]]; then
   export PYTHONHTTPSVERIFY=0
 fi
 
@@ -13,12 +13,12 @@ export LOGTRUST_CONF=${LOGTRUST_CONF:-/etc/logtrust}
 
 $LOGTRUST_HOME/relay/UpdateRelay.py  > /var/log/lt-relay.log 2>&1
 
-if [ -s $LOGTRUST_CONF/scoja/current/config-relay.tar ]
+if [[ -s $LOGTRUST_CONF/scoja/current/config-relay.tar ]]
 then
         tar  -C $LOGTRUST_CONF/scoja/current/ -xf $LOGTRUST_CONF/scoja/current/config-relay.tar
         rm -f $LOGTRUST_CONF/scoja/current/config-relay.tar
 
-        if [ -f $LOGTRUST_CONF/scoja/current/pass.temp ]; then
+        if [[ -f $LOGTRUST_CONF/scoja/current/pass.temp ]]; then
           newpass=`cat $LOGTRUST_CONF/scoja/current/pass.temp |cut -d"=" -f2|sed 's/\//'"\\\\\\\\\/"'/g'`
           sed -i -e 's/keypass = \".*\"/keypass = \"'"${newpass}"'\"/' $LOGTRUST_CONF/scoja/current/local.py
           cp /etc/logtrust/scoja/current/pass.temp /opt/devo/relay-data
